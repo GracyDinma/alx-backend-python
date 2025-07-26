73628b3d7e627e77c3417b10f05dee43ddecbb20
GracyDinma <okohagchidinma@gmail.com>
2025-07-25 22:17:13 -0500

    raises exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,5 +18,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Assert raises context to test key error
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 if __name__ == "__main__":
     unittest.main()
