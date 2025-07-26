5a5ff27839da5a2c15506942540e183f8bd7daa1
GracyDinma <okohagchidinma@gmail.com>
2025-07-25 23:02:04 -0500

    json file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,10 @@
 """Test for access nested map in utils
 """
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from parameterized import parameterized
+from unittest.mock import patch, Mock
+import requests
 
 class TestAccessNestedMap(unittest.TestCase):
     """class of Test Access Nested Map
@@ -27,6 +29,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    """class that test for JSON from remote url
+    """
+
+    @parameterized.expand([
+        ("http://example.com", test_played={"payload": True}),
+        ("http://holberton.io", test_payload={"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, url: str, expected, mock_get):
+        """A method that test that utils.get_json
+        returns expected result.
+        """
+        mock_get.return_value.json.return_value = {"test_played"}
 
 if __name__ == "__main__":
     unittest.main()
