a8087c2e2dc5d421b1f7d44a10df879a73e73356
GracyDinma <okohagchidinma@gmail.com>
2025-11-15 01:07:34 +0000

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,9 @@ values from a nested dictionary with a sequence of keys.
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -35,6 +37,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test suite for get_json and ensures retrieval of expected result.
+    """
+    @patch('utils.requests.get')
+    def test_get_json(self, mock_get):
+        mock_get.return_value.json.return_value = {'test_url': "test_payload"}
+        data = requests.get("http://holberton.io").json()
+        assert data == {'test_url': "test_payload"}
+        mock_get.assert_called_once_with("http://holberton.io")
 
 
 if __name__ == "__main__":
