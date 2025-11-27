78c2638ebf5d6089dd22476e524ac5f7c4e5fa5e
GracyDinma <okohagchidinma@gmail.com>
2025-11-27 03:51:41 -0600

    Integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,6 +5,7 @@ A test suite for github org client.
 import unittest
 from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
+from unittest import TestCase
 from unittest.mock import patch, PropertyMock, Mock
 from utils import get_json
 from parameterized import parameterized
