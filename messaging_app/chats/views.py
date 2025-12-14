75afa67e2f2621d641921f75af0b40418fa497b0
GracyDinma <okohagchidinma@gmail.com>
2025-12-14 13:06:39 -0600

    added status

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
 from rest_framework.permissions import IsAuthenticated
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
