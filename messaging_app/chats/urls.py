8f288cfb7dfafa1d4069f2604e63ef18b5b4b2f6
GracyDinma <okohagchidinma@gmail.com>
2025-12-14 13:38:35 -0600

    adding api

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,24 @@
 from rest_framework.routers import routers
 from .views import ConversationViewSet, MessageViewSet
 from django.urls import path, include
+from rest_framework_nested.routers import NestedDefaultRouter
 
 
 router = routers.DefaultRouter()
-router.register(r"conversation", ConversationViewSet, basename="converation")
-router.register(r"messages", MessageViewSet, basename="message")
+router.register(r"conversation", ConversationViewSet, basename="conversation")
+conversations_router = NestedDefaultRouter(
+    router,
+    r"conversations",
+    lookup="conversation"
+)
+
+conversations_router.register(
+    r"messages",
+    MessageViewSet,
+    basename="conversation-messages"
+)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(conversations_router.urls)),
 ]
