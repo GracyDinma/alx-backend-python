1d1daf4b5743b4238faea8fd49b2499609422989
GracyDinma <okohagchidinma@gmail.com>
2025-12-14 13:22:03 -0600

    fixing routers

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,7 +3,7 @@ from .views import ConversationViewSet, MessageViewSet
 from django.urls import path, include
 
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r"conversation", ConversationViewSet, basename="converation")
 router.register(r"messages", MessageViewSet, basename="message")
 
